afa05f667d53458dc4cb2952e7df11af717049ae
brianTopGun <363551751@qq.com>
2022-10-17 12:58:27 +0800

    new webproject

---
+++ servletDemo/src/main/java/com/servlet/servletdemo/test.java
@@ -0,0 +1,20 @@
+package com.servlet.servletdemo;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("我是java开发工程师");
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(i+" * "+j+"="+i*j+" ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+
+}
